4ebb2cd17bb3046e08993a878029f6c8386461f5
C. Daniel Chase <dan@cdchase.com>
2019-07-27 12:00:20 -0400

    Updating old changes to fix repo errors.

--- git/completion.sh
+++ git/completion.sh
@@ -1,7 +1,8 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+brewPrefix=`/usr/local/Homebrew/bin/brew --prefix`
+completion=${brewPrefix}/etc/bash_completion.d/git-completion.bash
 
 if test -f $completion
 then
